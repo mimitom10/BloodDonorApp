584d9b8d0b5548c07ec93cb6dafade056d9cbb9d
mimitom10 <mimitom14@abv.bg>
2020-04-21 01:13:42 +0300

    Assign Administrator Role to User

---
+++ Data/BloodDonor.Data/Seeding/UserSeeder.cs
@@ -0,0 +1,36 @@
+﻿using BloodDonor.Common;
+using BloodDonor.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodDonor.Data.Seeding
+{
+    public class UserSeeder : ISeeder
+    {
+        private const string adminEmail = "admin@abv.bg";
+        private const string adminPass = "Admin123456Admin";
+        private const string adminRole = "Administrator";
+
+
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var user = new ApplicationUser
+            {
+                UserName = adminEmail,
+                Email = adminEmail,
+            };
+
+            var result = await userManager.CreateAsync(user, adminPass);
+
+
+            await userManager.AddToRoleAsync(user, adminRole);
+            //    await dbContext.Categories.AddAsync(new Category
+        }
+    }
+}
---
+++ Services/BloodDonor.Services.Data/ResponsesServices/ResponsesService.cs
@@ -0,0 +1,34 @@
+﻿using BloodDonor.Data.Common.Repositories;
+using BloodDonor.Data.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodDonor.Services.Data.ResponsesServices
+{
+    public class ResponsesService : IResponsesService
+    {
+        private readonly IDeletableEntityRepository<Response> responsesRepository;
+
+        public ResponsesService(IDeletableEntityRepository<Response> responsesRepository)
+        {
+            this.responsesRepository = responsesRepository;
+        }
+
+        public async Task<string> AddAsync(string details, bool isAnonymous, string donorId, string requestId)
+        {
+            var response = new Response
+            {
+                Details = details,
+                IsAnonymous = isAnonymous,
+                DonorId = donorId,
+                RequestId = requestId,
+            };
+            await this.responsesRepository.AddAsync(response);
+            await this.responsesRepository.SaveChangesAsync();
+            return response.Id;
+        }
+    }
+}
